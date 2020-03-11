69a459dd97d42159a4be1ec732efe15622bfe0e4
Viktor Sjöblom <Viktorsjoblom93@gmail.com>
2020-03-11 11:43:00 +0100

    add structure

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require __DIR__ . '/views/header.php'; ?>
+
+<div class="logo-container">
+    <p class="metal-hand">🤘</p>
+    <img class="main-logo" src="/assets/logo.png">
+    <p class="metal-hand">🤘</p>
+</div>
+
+<?php require __DIR__ . '/views/footer.php'; ?>
